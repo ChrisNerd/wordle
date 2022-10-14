00673f99881d0c43e2ac9246f5abc1315c4336e7
Chris Shannon <chris.shannon@gmail.com>
2022-10-14 17:58:00 -0600

    Update Wordle3.java
    The 5 for loops can each be terminated early.
    The word list is sorted lexicographically.
    If the words are ABCD and E, and A > B > C > D > E. Once |A| has 24 or fewer bits, then so do B,C,D and E. So the maximum of any union can only be 24.
    This is known and implemented in everyone's version.
    But the other loops can be optimized similarly. Once |B| has 19 or fewer bits, then so does the maximum union of B, C, D and E. So B can be exited early. Likewise, no need to check C once it has 14 or few bits; D must have 9 or more and E must have 4 or more. The case with E is irrelavent because all elements have 5 bits.
    The indices of where these the transitions from say, 25 to 24 bits occurs is found using the insertion point returned (but not found) of the Arrays.binarySearch function. A search for 1 << 24 returns a negative number (indicating it was not found as expected) but its insertion point is the first number less than that.
    BinarySearch only works if the array is sorted ascendingly, but we want our array sorted descendingly. So we adjust the insertion points by subtracting them from the length and reverse the array after we've done the searches.
    
    The optimization on the parallel loop of A is interesting. The cost of a loop around A=words[0] is much higher than A=words[i] due to the time complexity being on the order of (N-i) choose 4. So if we're looking to divide up the cost of the first loop only, we shouldn't do it with an arithmetic sequence. But either a round robin or something like size^(i/(k+1)), where k is the number of threads.
    Overall the algorithm takes (n choose 5) * p^4, where p is the probability that two words chosen at random will not contain any mutual letters. I calculated this to be about 0.28.
    I calculated this by taking a frequency table of all the english letters and putting them in a spreadsheet. For each letter I calculated:
    Probability of Letter, p (for E => 0.12, T => .09 ...)
    Probability of not letter 1-p
    5 letters all not (assuming independence which is not valid but let's go with it!) (1-p)^5
    Word containing at least 1 letter, (complement of above)
    2 words containing at least 1 letter, (square of above)
    2 words don't contain at least 1 letter, (complement of above)
    Now we're looking for the probability that 
    2 words don't contain at least 1 a AND
    2 words don't contain at least 1 b AND
    2 words don't contain at least 1 c AND...
    So we multiply them all out to get 0.28
    This isn't too far off from the number of cliques of various sizes calculated: 
    number of cliques of size(0,1,2,3,4,5)
    1, 5977, 3213696, 95866204, 26133319, 538
    The 0th term is included as the empty set.
    p is 3213696 / (5977 choose 2) = 0.18.
    
    Incidentally the "Parker Algorithm" is soooo slow not because he's using Sets instead of Ints, but because he has a pairs vs pairs loop which takes
    ( n choose 2 * p) choose 2 = 5.16391938336 × 10^12
    
    If we include the early termination of the A loop we get number of cliques of size i =
    1, 460, 554861, 29450403, 14756251, 538
    
    I also used groupingBy to handle anagrams, as it's the perfect tool for the job. GroupingBy uses a function String=>Int which he have. When Ints "collide", they are grouped into a list. The result is a mapping from Ints to List[String], as desired. The keys (ints) of this map is easily extracted then sorted to be the list of words to work with. When we go to print a word, all the anagrams are easily accessed by map[word] which returns the list.

--- Wordle3.java
+++ Wordle3.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.Map;
+import java.util.Set;
 
 import java.time.Duration;
 import java.time.Instant;
@@ -47,19 +49,16 @@ ABCDEFGHIJKLMNOPQRSTUVWXYZ
 --CD-FG--JK---O--RS-U----- fjord OR gucks (UNION)
 */
 
-class Wordle {
+public class Wordle {
     private static List<String> rawWords;
+    private static Map<Integer, List<String>> groupByEncode;
 
     public static void main(String[] args) throws IOException {
         Stopwatch stopwatch = new Stopwatch();
 
         rawWords = new ArrayList<>();
-        // Uncomment the following 1 line to find 538 solutions:
-        // rawWords.addAll(Files.readAllLines(Path.of("words_alpha.txt")));
-        // Uncomment the following 2 lines to find 10 solutions:
-        rawWords.addAll(Files.readAllLines(Path.of("wordle-nyt-answers-alphabetical.txt")));
-        rawWords.addAll(Files.readAllLines(Path.of("wordle-nyt-allowed-guesses.txt")));
-        rawWords.removeIf(word -> word.length() != 5);
+	//        rawWords.addAll(Files.readAllLines(Path.of("wordle-answers-alphabetical.txt")));
+        rawWords.addAll(Files.readAllLines(Path.of("../wordle-nyt-allowed-guesses.txt")));
         System.out.println(rawWords.size() + " raw words");
 
         int[] cookedWords = rawWords.stream()
@@ -75,17 +74,80 @@ class Wordle {
                 .toArray();
         System.out.println(cookedWords.length + " cooked words\n");
 
+
+
+	/*Map<BigDecimal, List<Item>> groupByPriceMap = 
+			items.stream().collect(Collectors.groupingBy(Item::getPrice));
+	*/
+ groupByEncode =   rawWords.stream().collect(Collectors.groupingBy(Wordle::encodeWord));
+  int[] cookedList = groupByEncode.keySet().stream()
+    .mapToInt(Integer::intValue)
+      .filter(word -> Integer.bitCount(word) == 5)
+                .sorted()
+                // remove anagrams
+                .distinct()
+   .toArray(); 
+
+  groupByEncode.forEach((key, value) -> System.out.println(String.format("%32s", Integer.toBinaryString(key)).replace(' ', '0')  + "==" + key + ":" + value));
+
+	System.out.println(Arrays.binarySearch(cookedWords, 1 << 24));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 19));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 14));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 9));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 4));
+        System.out.println(Arrays.binarySearch(cookedWords, 0));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 27));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 26));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 25));
+        System.out.println(Arrays.binarySearch(cookedWords, 1 << 23));
+	/*
+b =	(-(insertion point) - 1)
+b =	-i - 1
+b+1 =	-i
+-b-1 =	i
+	*/
+	int maxA = cookedWords.length + 1 + Arrays.binarySearch(cookedWords, 1 << 24);
+	int maxB = cookedWords.length + 1 + Arrays.binarySearch(cookedWords, 1 << 19);
+	int maxC = cookedWords.length + 1 + Arrays.binarySearch(cookedWords, 1 << 14);
+	int maxD = cookedWords.length + 1 + Arrays.binarySearch(cookedWords, 1 << 9);
+	int maxE = cookedWords.length + 1 + Arrays.binarySearch(cookedWords, 1 << 4) ;
+	for(int i = 0; i < cookedWords.length / 2; i++)
+	    {
+		int temp = cookedWords[i];
+		cookedWords[i] = cookedWords[cookedWords.length - i - 1];
+		cookedWords[cookedWords.length - i - 1] = temp;
+	    }
+        System.out.println(maxA);
+        System.out.println(maxB);
+        System.out.println(maxC);
+        System.out.println(maxD);
+        System.out.println(maxE);
+        System.out.println(Integer.toBinaryString(cookedWords[197]));
+        System.out.println(Integer.toBinaryString(cookedWords[196]));
+        System.out.println(Integer.toBinaryString(cookedWords[198]));
+        System.out.println("ab".charAt(0));
+        System.out.println(1 << "ab".charAt(0));
+        System.out.println(1 << 'a');
+        System.out.println(1 << 'b');
+        System.out.println(1 << 'c');
+        System.out.println('a');
+	/*		for(int i = 0; i < cookedWords.length; i++)
+	    System.out.println(Integer.toBinaryString(cookedWords[i]));
+	*/
+
+
+	
+
         // 54 MB  skip[i][j] is the first i-intersection-free index >= j
         short[][] skip = new short[cookedWords.length][cookedWords.length + 1];
-        // for (int i = 0; i < cookedWords.length; ++i) {
-        IntStream.range(0, cookedWords.length).parallel().forEach((int i) -> {
+        for (int i = 0; i < cookedWords.length; ++i) {
             skip[i][cookedWords.length] = (short) cookedWords.length; // 5176
             int A = cookedWords[i];
             for (int j = cookedWords.length - 1; j >= i; --j) {
                 int B = cookedWords[j];
                 skip[i][j] = ((A & B) == 0) ? (short) j : skip[i][j + 1];
             }
-        });
+        }
         // 20 KB  first[i] is identical to skip[i][i] but hot in cache
         int[] first = new int[cookedWords.length];
         for (int i = 0; i < cookedWords.length; ++i) {
@@ -93,40 +155,60 @@ class Wordle {
         }
 
         // for (int i = 0; i < cookedWords.length; ++i) {
-        IntStream.range(0, cookedWords.length).parallel().forEach((int i) -> {
-            int A = cookedWords[i];
+        IntStream.range(0, maxA).parallel().forEach((int i) -> {
 
             // for (int j = i + firstStep[i]; j < cookedWords.length; ++j) {
-            for (int j = first[i]; j < cookedWords.length; j++,
-                /* slurp     monty */                      j = skip[i][j]) {
-                int B = cookedWords[j];               // lurks  monty  zloty
-                int AB = A | B;
+		//            for (int j = first[i]; j < maxB; j++, j = skip[i][j]) {
+		for (int j = first[i]; j < maxB; j++, j = skip[i][j]) {
 
                 // for (int k = j + firstStep[j]; k < cookedWords.length; ++k) {
-                for (int k = first[j]; k < cookedWords.length; k++,
-                                                               k = skip[i][k],
-                                                               k = skip[j][k]) {
-                    int C = cookedWords[k];
-                    if ((AB & C) != 0) continue;
-                    int ABC = AB | C;
+                for (int k = first[j]; k < maxC; k++) {
+		    /*                    k = skip[i][k]; // this likely reduces the do-while loop by 1 iteration
+                    k = skip[j][k];
+                    */
+		    int oldK;
+		    
+                    do{
+			oldK = skip[i][k];
+                        k = skip[j][oldK];
+                    } while(k != oldK); // skip until i and j don't collide with k
+                    if (k == cookedWords.length) break; // skipped to end of k, next j
 
                     // for (int l = k + firstStep[k]; l < cookedWords.length; ++l) {
-                    for (int l = first[k]; l < cookedWords.length; l++,
-                                                                   l = skip[i][l],
-                                                                   l = skip[j][l],
-                                                                   l = skip[k][l]) {
-                        int D = cookedWords[l];
-                        if ((ABC & D) != 0) continue;
-                        int ABCD = ABC | D;
+                    for (int l = first[k]; l < maxD; l++) {
+			int oldL;
+                        do{
+                            oldL = skip[i][l];
+                            l = skip[j][oldL];
+                            l = skip[k][l];
+                        } while(l != oldL);
+                        if (l == cookedWords.length) break;
+
 
                         // for (int m = l + firstStep[l]; m < cookedWords.length; ++m) {
-                        for (int m = first[l]; m < cookedWords.length; m++,
-                                                                       m = skip[i][m],
-                                                                       m = skip[j][m],
-                                                                       m = skip[k][m],
-                                                                       m = skip[l][m]) {
+                        for (int m = first[l]; m < maxE; m++) {
+			    int oldM;
+                            do{
+				/*
+				  It's interesting to think about what kind of performance gain
+				  this whole skip matrix gets you over just interatively filter
+				  for a new list at every depth.
+				  This is looking for fixed points of stair case functions.
+
+				 */
+                                oldM = skip[i][m];
+                                m = skip[j][oldM];
+                                m = skip[k][m];
+                                m = skip[l][m];
+                            } while(m != oldM);
+                            if (m == cookedWords.length) break;
+
+                            // since the words are not needed for comparisson, only fetch them for printing
+                            int A = cookedWords[i];
+                            int B = cookedWords[j];               
+                            int C = cookedWords[k];
+                            int D = cookedWords[l];
                             int E = cookedWords[m];
-                            if ((ABCD & E) != 0) continue;
 
                             System.out.printf("%s\n%s\n\n",
                                     stopwatch.elapsedTime(),
@@ -141,8 +223,14 @@ class Wordle {
 
     private static String decodeWord(int word) {
         // --C-----I--L-----------XY- cylix/xylic
-        return rawWords.stream()
+        return
+	    /*
+Blah! Needless O(n)
+	      rawWords.stream()
                 .filter(raw -> encodeWord(raw) == word)
+	    */
+
+	groupByEncode.get(word).stream()
                 .collect(Collectors.joining("/", visualizeWord(word) + " ", ""));
     }
 
@@ -158,11 +246,38 @@ class Wordle {
     }
 
     private static int encodeWord(String raw) {
-        //    1 1   1    1  1         fjord
+	String letterFreq = "ETAOINSRHDLUCMFYWGPBVKXQJZ".toLowerCase();
+	//                   01234567890123456789012345
+	    // Want to make an array from int-> int
+	    
+	    /*
+	    Give it raw.charAt(i) - 'a' which is 'a' -> 0, 'b' -> 1...
+	    and it will return 0 for 'e'(5), 1 for 't'(20 or whatever).
+	    want an array that looks like [x,x,x,x,x,0,x,x,x,....1 (in index 20), x,x,x]
+can use
+	     */
+	//        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+	int[] mapFromCharToIndex = new int[]{2, 19, 12, 9, 0, 14, 17, 8, 4,24, 21,
+		10, 13, 5, 3, 18, 23, 7 , 6, 1, 11, 20, 16, 22, 15, 25};
+
+	    /* alphabet.stream().mapToInt( letter -> letterFreq.find( letter ) ) ;*/
+
+	    
+	//    1 1   1    1  1         fjord
         int bitset = 0;
         for (int i = 0; i < raw.length(); ++i) {
-            bitset |= 1 << 26 >> raw.charAt(i);
+	    //	                bitset |= 1 << raw.charAt(i);
+	    /* so charAt returns a char which gets converted to an ASCII
+'a' => 97
+'b' => 98
+...
+But the bit shift operator << is implementation-defined when n is > 32, but the common implementation
+is shift by n % 32. And 97%32 happens to be only 1, so a gets shifted by 1, even though we want to it by shifted by 0.
+
+	     */
+	               bitset |= 1 << mapFromCharToIndex[raw.charAt(i) - 'a'];
         }
+	//	            System.out.println(bitset + '\n');
         return bitset;
     }
 
@@ -172,7 +287,8 @@ class Wordle {
         char[] a = new char[26];
         word <<= 6;
         for (int i = 0; i < a.length; ++i, word <<= 1) {
-            a[i] = (word < 0) ? (char) ('A' + i) : '-';
+	    //            a[i] = (word < 0) ? (char) ('A' + i) : '-';
+	    a[i] = (word < 0) ? "ETAOINSRHDLUCMFYWGPBVKXQJZ".charAt(25-i) : '-';
         }
         return new String(a);
     }
@@ -188,3 +304,34 @@ class Stopwatch {
         return "[" + formatted + "] ";
     }
 }
+// Without modifications runs at [00:00:01.6ish]
+// with maxA,B,C,D,E gets to [00:00:00.274]
+/*
+E 	21912 	  	E 	12.02
+T 	16587 	  	T 	9.10
+A 	14810 	  	A 	8.12
+O 	14003 	  	O 	7.68
+I 	13318 	  	I 	7.31
+N 	12666 	  	N 	6.95
+S 	11450 	  	S 	6.28
+R 	10977 	  	R 	6.02
+H 	10795 	  	H 	5.92
+D 	7874 	  	D 	4.32
+L 	7253 	  	L 	3.98
+U 	5246 	  	U 	2.88
+C 	4943 	  	C 	2.71
+M 	4761 	  	M 	2.61
+F 	4200 	  	F 	2.30
+Y 	3853 	  	Y 	2.11
+W 	3819 	  	W 	2.09
+G 	3693 	  	G 	2.03
+P 	3316 	  	P 	1.82
+B 	2715 	  	B 	1.49
+V 	2019 	  	V 	1.11
+K 	1257 	  	K 	0.69
+X 	315 	  	X 	0.17
+Q 	205 	  	Q 	0.11
+J 	188 	  	J 	0.10
+Z 	128 	  	Z 	0.07
+
+*/
